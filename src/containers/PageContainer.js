5419f0fcd3da392eedfa23012b8a148a15daa8b3
actooalizame <gndalmedo@gmail.com>
2018-07-30 01:18:13 -0400

    minicleaned

--- src/containers/PageContainer.js
+++ src/containers/PageContainer.js
@@ -6,15 +6,10 @@ Meteor.connect('ws://192.168.1.83:3000/websocket');
 
 export default PageContainer = createContainer(props => {
 
-    //const carsHandle = Meteor.subscribe('cars.all');
-   	//const imageHandle = Meteor.subscribe('images.onlyOne');
    	const pagesHandle = Meteor.subscribe('pages.all');
     Meteor.subscribe('options.all');
     return {
         pagesReady: pagesHandle.ready(),
-        //imageReady: imageHandle.ready(),
-       // image: Meteor.collection('carImages').findOne({}, {sort: {createdAt: -1}})
+
     };
  }, Page);
-
-//playing: Meteor.collection('currentSongs').findOne({}, {sort: {createdAt: -1}})
